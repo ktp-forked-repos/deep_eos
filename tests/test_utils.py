37162689754a8efee17ea02eec86bf0f21132ca0
Stefan Schweter <stefan@schweter.it>
2018-12-15 12:04:32 +0100

    tests: add unit test for calculate_evaluation_metrics

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,4 @@
-from deep_eos.utils import get_char_context
+from deep_eos.utils import get_char_context, calculate_evaluation_metrics
 
 
 def test_get_char_context():
@@ -15,3 +15,27 @@ def test_get_char_context():
     assert contexts[2].context == "of.▁M"
     assert contexts[3].context == "tr.▁1"
     assert contexts[4].context == "14.▁▁"  # Right padding is ok?
+
+def test_calculate_evaluation_metrics():
+    #sentence = "Dr. Prof. Müller geht zur Universität an der Str. 14.\n"
+
+    # potential eos positions are on [2,8,48,52]
+
+    # Real eos positions
+    gold_positions = set([52])
+
+    # First system tags something wrong...
+    # tp = 52
+    # fp = 8, 48
+    # fn = {}
+    # precision: 1 / (1 + 2) = 1/3
+    # recall: 1 / (1 + 0) = 1.0
+    # f-score: 2 * (1/3 * 1) / (1/3 + 1) = 0.5
+    predicted_positions = set([8, 48, 52])
+
+    prec, rec, f_score = calculate_evaluation_metrics(gold_positions=gold_positions,
+                                                      predicted_positions=predicted_positions)
+
+    assert(prec == 1/3)
+    assert(rec == 1.0)
+    assert(f_score == 0.5)
