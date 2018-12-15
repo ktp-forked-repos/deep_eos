2129a0076d9f4ae7464e3e002f9f4952cd14bec5
Stefan Schweter <stefan@schweter.it>
2018-12-15 11:34:18 +0100

    tests: update padding symbol in unit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -9,11 +9,9 @@ def test_get_char_context():
 
     contexts = list(get_char_context(left_window=left_ws, right_window=right_ws, buffer=buffer))
 
-    print(contexts)
-
     assert len(contexts) == 5
-    assert contexts[0].context == "  . D"  # Left padding is ok?
-    assert contexts[1].context == "Dr. P"
-    assert contexts[2].context == "of. M"
-    assert contexts[3].context == "tr. 1"
-    assert contexts[4].context == "14.  "  # Right padding is ok?
+    assert contexts[0].context == "▁▁.▁D"  # Left padding is ok?
+    assert contexts[1].context == "Dr.▁P"
+    assert contexts[2].context == "of.▁M"
+    assert contexts[3].context == "tr.▁1"
+    assert contexts[4].context == "14.▁▁"  # Right padding is ok?
