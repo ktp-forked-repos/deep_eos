36f94f72fd7d92a9a072d3cf228dfdc2a56e4ce5
Stefan Schweter <stefan@schweter.it>
2019-02-10 09:48:11 +0100

    utils: move all file and url related functions to new file_utils helper class

--- deep_eos/utils.py
+++ deep_eos/utils.py
@@ -1,16 +1,7 @@
 """Implement various helper functions."""
 
-import logging
-import os
-import shutil
-import tempfile
 from collections import namedtuple
-from pathlib import Path
 from typing import Set
-from urllib.parse import urlparse
-
-import requests
-import tqdm
 
 from deep_eos.data import Context
 
@@ -18,9 +9,7 @@ EOS_CHARS = ['.', ';', '!', '?', ':']
 EOS_MARKER = '</eos>'
 SEPS = [' ', '\n']
 EvaluationResult = namedtuple("EvaluationResult", 'precision, recall, f_score')
-CACHE_ROOT = os.path.expanduser(os.path.join('~', '.deep_eos'))
 
-LOG = logging.getLogger(__name__)
 
 def get_char_context(left_window, right_window, buffer):
     """Implement method for fetching left and right context around a potential end-of-sentence.
@@ -102,51 +91,3 @@ def calculate_evaluation_metrics(gold_positions: Set[int], predicted_positions: 
     f_score = 2 * (precision * recall) / (precision + recall)
 
     return EvaluationResult(precision=precision, recall=recall, f_score=f_score)
-
-
-def cached_path(url: str, cache_dir: Path) -> Path:
-    """Perform caching of a specific url for a given path.
-
-    If the local path does not exist, download file from url.
-
-    :param url: url of file
-    :param cache_dir: local cache dir
-    :return: path of local file
-    """
-    dataset_cache = Path(CACHE_ROOT) / cache_dir
-    parsed = urlparse(url)
-
-    if parsed.scheme in ['https']:
-
-        dataset_cache.mkdir(parents=True, exist_ok=True)
-
-        cache_path = dataset_cache / url.split('/')[-1]
-
-        if cache_path.exists():
-            return cache_path
-
-        response = requests.head(url)
-
-        if not response:
-            LOG.error(f'Could not retrieve model from {url}')
-
-        _, temp_filename = tempfile.mkstemp()
-
-        LOG.info(f'{url} not found in cache, downloading it to {temp_filename}')
-
-        req = requests.get(url, stream=True)
-        file_size = int(req.headers.get('Content-Length'))
-        chunk_size = 1024
-        num_bars = int(file_size / chunk_size)
-
-        with open(temp_filename, 'wb') as temp_file:
-            for chunk in tqdm.tqdm(req.iter_content(chunk_size=chunk_size),
-                                   total=num_bars,
-                                   unit='B',
-                                   leave=True):
-                temp_file.write(chunk)
-
-        shutil.copyfile(temp_filename, str(cache_path))
-        os.remove(temp_filename)
-
-        return cache_path
